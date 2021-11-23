d0e8c67502b66f2de8b36cf9fa204e6546a29060
mariordgez <mariordgez@hotmail.com>
2021-11-23 10:06:34 -0700

    Created reservation form and api post action

--- src/components/Reservations/reservationForm.js
+++ src/components/Reservations/reservationForm.js
@@ -2,29 +2,53 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import '../../css/reservation.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
-import { formToggle } from '../../redux/reservations/reservationReducer';
+import { updateFetchReservationList } from '../../redux/reservations/reservationReducer';
 
 const ReservationForm = () => {
-  const { userName } = useSelector((state) => state.loginPage);
+  const { userId } = useSelector((state) => state.loginPage);
+  const { cars } = useSelector((state) => state.carList);
   const dispatch = useDispatch();
-  const reservationForm = () => {
-    dispatch(formToggle());
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    const reservationData = new FormData(event.target);
+    const body = {
+      user_id: userId,
+      car_id: Number(reservationData.get('cars')),
+      city: reservationData.get('city'),
+      date: reservationData.get('date'),
+    };
+    console.log(body);
+
+    dispatch(updateFetchReservationList(body));
   };
 
   return (
     <>
-      <Card.Title className="text-align big-title">
-        Hello &nbsp;
-        {userName}
-        !, here you can see your scheduled reservations:
-      </Card.Title>
-      <Card.Text className="py-5">
-        <button type="button" onClick={reservationForm}>
-          {' '}
-          Activate Lasers
-        </button>
-      </Card.Text>
+      <form className="" name="add_car" action="post" onSubmit={submitForm}>
+        <br />
+        <select id="cars" name="cars">
+          {cars.map((car) => (
+            <option key={car.id} value={car.id}>
+              {car.model}
+              {' '}
+              {car.brand}
+              {' '}
+              {car.name}
+            </option>
+          ))}
+        </select>
+        <br />
+        <label htmlFor="city">
+          <input type="text" name="city" id="city" placeholder="City" required />
+        </label>
+        <br />
+        <label htmlFor="date">
+          <input type="date" name="date" id="date" placeholder="Date" required />
+        </label>
+        <br />
+        <input type="submit" className="" />
+      </form>
     </>
   );
 };
