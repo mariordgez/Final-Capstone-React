23bd139cc833e192b45230000c6929b3afcc6682
mariordgez <mariordgez@hotmail.com>
2021-11-23 05:29:35 -0700

    Made bg responsive

--- src/components/Reservations/myReservations.js
+++ src/components/Reservations/myReservations.js
@@ -1,25 +1,32 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import '../../css/reservation.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import { fetchReservationList } from '../../redux/reservations/reservationReducer';
 
 const MyReservation = () => {
-  const { reservationList } = useSelector((state) => state.reservationList);
+  const { reservations } = useSelector((state) => state.reservationList);
+  const { userName, userId } = useSelector((state) => state.loginPage);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchReservationList());
-    console.log(reservationList);
+    console.log(userId);
+    console.log(reservations);
   }, []);
   return (
     <Card className="bg-dark text-white h-100">
       <Card.Img
+        className="reservation-bg"
         src=" https://drive.google.com/uc?export=view&id=1JvXmF5rPm3dkeuzda-VK4g6U5iXku3bO"
         alt="Card image"
-        fluid
       />
       <Card.ImgOverlay>
-        <Card.Title>Card title</Card.Title>
+        <Card.Title>
+          Hello &nbsp;
+          {userName}
+          !, here you can see your scheduled reservations:
+        </Card.Title>
         <Card.Text>
           This is a wider card with supporting text below as a natural lead-in to additional
           content. This content is a little bit longer.
