d2548f7fdc5483c7559e2c475ece37e46b5356d8
mariordgez <mariordgez@hotmail.com>
2021-11-23 13:50:56 -0700

    Add cancel reservation form

--- src/components/Reservations/reservationForm.js
+++ src/components/Reservations/reservationForm.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import '../../css/reservation.css';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { updateFetchReservationList } from '../../redux/reservations/reservationReducer';
+import { updateFetchReservationList, formToggle } from '../../redux/reservations/reservationReducer';
 
 const ReservationForm = () => {
   const { userId } = useSelector((state) => state.loginPage);
   const { cars } = useSelector((state) => state.carList);
   const dispatch = useDispatch();
-
+  const cancelForm = (event) => {
+    event.preventDefault();
+    dispatch(formToggle());
+  };
   const submitForm = (event) => {
     event.preventDefault();
     const reservationData = new FormData(event.target);
@@ -19,35 +23,54 @@ const ReservationForm = () => {
       date: reservationData.get('date'),
     };
     JSON.stringify(body);
-
+    dispatch(formToggle());
     dispatch(updateFetchReservationList(body));
   };
 
   return (
     <>
-      <form className="" name="add_car" action="post" onSubmit={submitForm}>
+      <h1 className="text-align">Fill out the reservation form</h1>
+      <form className="text-align" name="add_car" action="post" onSubmit={submitForm}>
         <br />
-        <select id="cars" name="cars">
-          {cars.map((car) => (
-            <option key={car.id} value={car.id}>
-              {car.model}
-              {' '}
-              {car.brand}
-              {' '}
-              {car.name}
-            </option>
-          ))}
-        </select>
-        <br />
-        <label htmlFor="city">
-          <input type="text" name="city" id="city" placeholder="City" required />
+        <label htmlFor="cars">
+          <h3>Select your test car:  </h3>
+          <select className="" id="cars" name="cars" placeholder="Select a car">
+            {cars.map((car) => (
+              <option key={car.id} value={car.id}>
+                {car.model}
+                {' '}
+                {car.brand}
+                {' '}
+                {car.name}
+              </option>
+            ))}
+          </select>
         </label>
         <br />
-        <label htmlFor="date">
+        <label className="py-3" htmlFor="city">
+          <h3>Please select a city:</h3>
+          <input className="" type="text" name="city" id="city" placeholder="City" required />
+        </label>
+        <br />
+        <label className="" htmlFor="date">
+          <h3>Select an available date: </h3>
           <input type="date" name="date" id="date" placeholder="Date" required />
         </label>
         <br />
-        <input type="submit" className="" />
+        <div className="py-3">
+          <Button
+            type="submit"
+            className="mx-2"
+            variant="success"
+            size="lg"
+          >
+            Create reservation
+          </Button>
+
+          <Button type="button" variant="danger" size="lg" onClick={cancelForm}>
+            Cancel reservation
+          </Button>
+        </div>
       </form>
     </>
   );
