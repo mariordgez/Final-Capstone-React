85f527e3f901008b49fc524dd85e50858e239ae7
mariordgez <mariordgez@hotmail.com>
2021-11-23 13:50:56 -0700

    Add cancel reservation form

--- src/components/Reservations/reservationButton.js
+++ src/components/Reservations/reservationButton.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import '../../css/reservation.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import { formToggle } from '../../redux/reservations/reservationReducer';
 
 const ReservationButton = () => {
-  const { userName } = useSelector((state) => state.loginPage);
   const dispatch = useDispatch();
   const reservationForm = () => {
     dispatch(formToggle());
@@ -14,16 +14,11 @@ const ReservationButton = () => {
 
   return (
     <>
-      <Card.Title className="text-align big-title">
-        Hello &nbsp;
-        {userName}
-        !, here you can see your scheduled reservations:
-      </Card.Title>
+      <Card.Title className="text-align big-title">Welcome to test a car!</Card.Title>
       <Card.Text className="py-5">
-        <button type="button" onClick={reservationForm}>
-          {' '}
-          Activate Lasers
-        </button>
+        <Button variant="primary" size="lg" onClick={reservationForm}>
+          Reserve a test drive
+        </Button>
       </Card.Text>
     </>
   );
